c8113b8742f7c5784fd22eb2e704fdb09cb8349c
dffailhmfkri <dilhamfik1@gmail.com>
2024-04-27 15:20:58 +0700

    TRK3-9 Progres Fitur FAQ

---
+++ TUBES_RPL_Kelompok318/resources/views/faq.blade.php
@@ -0,0 +1,90 @@
+
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1" />
+    <meta name="csrf-token" content="fn1341lDU85Q1hplvgQRL1O84Z3i2mRfH2j8guWj" />
+    <meta name="events-id" content="bJcZH0WmDxS0SsyTWPYnIxrBYqawMwQj87qyTRz0" />
+    <!-- Favicon -->
+    <link href="img/favicon.ico" rel="icon">
+    <!-- Google Web Fonts -->
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Heebo:wght@400;500;600&family=Nunito:wght@600;700;800&family=Pacifico&display=swap" rel="stylesheet">
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="{{asset('/css/faq.css') }}">
+    <title>FAQ Feater Catering</title>
+    <!-- Icon Font Stylesheet -->
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.10.0/css/all.min.css" rel="stylesheet">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.4.1/font/bootstrap-icons.css" rel="stylesheet">
+    <!-- Customized Bootstrap Stylesheet -->
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+    <!-- Template Stylesheet -->
+    <link href="css/style1.css" rel="stylesheet">
+  </head>
+  <body>
+<!-- Navbar -->
+        <x-navbar/>
+<!-- END: Header-->
+
+<!-- MODAL -->
+<x-modal/>
+<div class="faq-image"></div>
+  <div class="faq-menu">
+    <div class="container px-lg-5"> <br>
+      <div class="faq-program" id="faq-weightloss">
+        <ol class="text-warning fst-italic">
+          <li>Seperti apa menu Feater Catering itu?</li>
+          <p>Menu Feater Catering diadaptasi dari menu <span class="fst-italic">fine dining</span> yang mewah dari berbagai restoran di penjuru dunia dan kemudian dibuat menjadi makanan diet dan <span class="fst-italic">healthy</span> yang dibuat oleh Chef Profesional yang terbaik di Jakarta dan sudah berpengalaman dari restoran bintang 5 baik di luar dan dalam negeri.</p>
+          <li>Apakah menu Feater Catering terjamin nutrisinya?</li>
+          <p>Menu yang tersedia di Feater Catering dipantau dan didukung penuh oleh dokter spesialis gizi dan Nutrisionis terbaik di Jakarta.</p>
+          <li>Berapa banyak <span class="fst-italic">customer</span> yang sudah berhasil berlangganan Feater Catering</li>
+          <p>Sudah ratusan ribu orang berhasil menurunkan berat badannya dengan Feater Catering.</p>
+          <li>Apa saja isi dalam satu <span class="fst-italic">box</span> Feater Catering?</li>
+          <p>Menu setiap <span class="fst-italic">box</span> mengandung nilai kalori yang rendah yaitu sekitar 400-500 kalori, dengan proporsi</p>
+          <div class="margin-box">
+            <ul class="text-white fst-normal">
+              <li>Karbohidrat Kompleks : Nasi Merah, nasi hitam, nasi coklat, pasta, jagung dsb</li>
+              <li>Protein Hewani Rendah Lemak : Ikan laut segar, ayam bagian dada tanpa kulit, daging tanpa lemak</li>
+              <li>Lemak Tak jenuh : Minyak salad/<span class="fst-italic">salad oil</span> dan minyak zaitun/<span class="fst-italic">olive oil</span></li>
+              <li>Sayuran : Kangkung, Bayam, Brokoli, Wortel, Tomat dll</li>
+              <li>Minim Gula, Garam dan Lemak jenuh</li>
+            </ul>
+          </div>
+      </div>
+    </div>
+  </div>
+  <div class="faq-yellow">
+    <div class="container">
+      <div class="row text-center">
+        <div class="col-lg-5 p-5">
+          <p class="fw-bold">(DISCOUNT IDR 70K) <br><br> UNTUK PEMBELIAN DUA ORANG ATAU LEBIH KE ALAMAT YANG SAMA</p>
+        </div>
+        <div class="col-lg-2">
+          <div class = "vertical-faq d-none d-md-none d-lg-block"></div>
+          <div class = "horizontal-faq d-block d-lg-none"></div>
+        </div>
+        <div class="col-lg-5 p-5">
+          <h2 class="fw-bold mt-4">FREE DELIVERY</h2>
+        </div>
+      </div>
+    </div>
+  </div>
+    <!-- Footer Start -->
+    <x-footer/>
+    <!-- Footer End -->
+    <!-- JavaScript Libraries -->
+    <script src="https://code.jquery.com/jquery-3.4.1.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0/dist/js/bootstrap.bundle.min.js"></script>
+    <script src="lib/wow/wow.min.js"></script>
+    <script src="lib/easing/easing.min.js"></script>
+    <script src="lib/waypoints/waypoints.min.js"></script>
+    <script src="lib/counterup/counterup.min.js"></script>
+    <script src="lib/owlcarousel/owl.carousel.min.js"></script>
+    <script src="lib/tempusdominus/js/moment.min.js"></script>
+    <script src="lib/tempusdominus/js/moment-timezone.min.js"></script>
+    <script src="lib/tempusdominus/js/tempusdominus-bootstrap-4.min.js"></script>
+  </body>
+</html>
