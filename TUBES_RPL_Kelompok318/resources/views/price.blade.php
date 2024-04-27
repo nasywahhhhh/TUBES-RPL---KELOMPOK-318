2f1ac99a992cf1b03a34e275883afc013e745ac1
bryohabs <bryohabs@student.telkomuniversity.ac.id>
2024-04-27 15:14:31 +0700

    TRK3-16 Menu Promo

---
+++ TUBES_RPL_Kelompok318/resources/views/price.blade.php
@@ -0,0 +1,232 @@
+
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1" />
+    <meta name="events-id" content="f2lCzlJzCE3W5c2t2KwUyiVPR3Dltype2wvFSTYh" />
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://yellowfitkitchen.com/frontend/css/bootstrap.min.css" />
+    <link rel="stylesheet" href="https://unpkg.com/aos@next/dist/aos.css" />
+    <link rel="stylesheet" href="https://yellowfitkitchen.com/frontend/css/owl.carousel.min.css">
+    <link rel="stylesheet" href="https://yellowfitkitchen.com/frontend/css/owl.theme.default.min.css">
+    <title>Promo Feater Catering</title>
+    <!-- Icon Font Stylesheet -->
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.10.0/css/all.min.css" rel="stylesheet">
+    <!-- Favicon -->
+    <link href="img/favicon.ico" rel="icon">
+    <!-- Customized Bootstrap Stylesheet -->
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+    <!-- Template Stylesheet -->
+    <link href="css/style1.css" rel="stylesheet">
+    <meta name="description" content="Program YellowFit Menawarkan Solusi Mudah Bagi Masyarakat Indonesia Untuk Diet Dan Hidup Sehat | Jaminan Uang Kembali 100% | Nutritionist approved | Affordable Price | Free Delivery Fee | Rasa Dijamin Lebih Enak | Konsultasi Gratis">
+
+
+    <style>
+      .first_button {
+        animation: shake 0.82s cubic-bezier(.36, .07, .19, .97) both infinite;
+        transform: translate3d(0, 0, 0);
+        backface-visibility: hidden;
+        perspective: 1000px;
+      }
+      @keyframes  shake {
+        10%, 90% { transform: translate3d(-1px, 0, 0); }
+        20%, 80% { transform: translate3d(2px, 0, 0); }
+        30%, 50%, 70% { transform: translate3d(-4px, 0, 0); }
+        40%, 60% { transform: translate3d(4px, 0, 0); }
+      }
+    </style>
+
+
+  </head>
+  <body>
+    <!-- BEGIN: Header-->
+<!-- Navbar -->
+<x-navbar/>
+<!-- END: Header-->
+
+<!-- MODAL -->
+<x-modal/>
+<div class="pricelist-image">
+  <div class="banner-text-pricelist text-white text-center">
+    <h1 class="fst-italic text-white">Price list</h1>
+  </div>
+</div>
+<div class="main-pricelist">
+  <div class="button-pricelist mb-3">
+    <div class="container">
+      <div class="row justify-content-around">
+      </div>
+    </div>
+  </div>
+
+  <div class="container px-lg-5" id="pricelist-weightloss">
+    <div class="container mt-3">
+      <div class="row justify-content-between">
+        <div class="col-lg-12 mx-auto text-center border-pricelist py-2">
+          <span class="text-warning fw-bold">1. LUNCH / DINNER PACKAGE</span>
+        </div>
+      </div>
+    </div>
+    <div class="row justify-content-between pt-3">
+      <div class="col-6 border-right-yellow">
+        <div class="weekly text-center">
+          <span class="text-warning fw-bold title-text-pricelist">WEEKLY</span>
+          <h3 style="letter-spacing:3px" class="text-white text-center text-pricelist">6 DAYS</h3>
+          <hr class="mx-auto line-pricelist">
+          <div class="text-white text-center">
+            <span class="fw-bold text-pricelist">LUNCH / DINNER</span><br>
+            <table class="table" style="border:none !important; border-style:hidden !important;">
+              <tbody>
+                <tr>
+                  <td>
+                    <span class="text-warning text-promo fw-light margin-promo-icon"><span class="text-white">IDR</span> 450.000</span><br>
+                    <span class="text-warning text-pricelist2 margin-promo-icon"><span class="small-text">IDR</span> 399.000</span>
+                  </td>
+                  <td>
+                    <img src="img/promo.png" class="img-promo">
+                  </td>
+                </tr>
+              </tbody>
+            </table>
+          </div>
+        </div>
+      </div>
+      <div class="col-6">
+        <div class="monthly text-center">
+          <span class="text-warning fw-bold title-text-pricelist">MONTHLY</span>
+          <h3 style="letter-spacing:3px" class="text-white text-center text-pricelist">25 DAYS</h3>
+          <hr class="mx-auto line-pricelist">
+          <div class="text-white text-center">
+            <span class="fw-bold text-pricelist">LUNCH / DINNER</span><br>
+            <table class="table" style="border:none !important; border-style:hidden !important;">
+              <tbody>
+                <tr>
+                  <td>
+                    <span class="text-warning text-promo fw-light margin-promo-icon"><span class="text-white">IDR</span> 2.000.000</span><br>
+                    <span class="text-warning text-pricelist2 margin-promo-icon"><span class="small-text">IDR</span> 1.799.000</span>
+                  </td>
+                  <td>
+                    <img src="img/promo.png" class="img-promo">
+                  </td>
+                </tr>
+              </tbody>
+            </table>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="container mt-5">
+      <div class="row justify-content-between">
+        <div class="col-lg-12 mx-auto text-center border-pricelist py-2">
+          <span class="text-warning fw-bold">2. LUNCH & DINNER PACKAGE PLUS</span>
+        </div>
+      </div>
+    </div>
+    <div class="container px-lg-5" id="pricelist-weightloss">
+      <div class="row justify-content-between pt-3">
+        <div class="col-6 border-right-yellow">
+          <div class="weekly text-center">
+            <span class="text-warning fw-bold title-text-pricelist">WEEKLY</span>
+            <h3 style="letter-spacing:3px" class="text-white text-center text-pricelist">6 DAYS</h3>
+            <hr class="mx-auto line-pricelist">
+            <div class="text-white text-center">
+              <span class="fw-bold text-pricelist">LUNCH & DINNER</span><br>
+              <table class="table" style="border:none !important; border-style:hidden !important;">
+                <tbody>
+                  <tr>
+                    <td>
+                      <span class="text-warning text-promo fw-light margin-promo-icon"><span class="text-white">IDR</span> 700.000</span><br>
+                      <span class="text-warning text-pricelist2 margin-promo-icon"><span class="small-text">IDR</span> 599.000</span>
+                    </td>
+                    <td>
+                      <img src="img/promo.png" class="img-promo">
+                    </td>
+                  </tr>
+                </tbody>
+              </table>
+            </div>
+          </div>
+        </div>
+        <div class="col-6">
+          <div class="monthly text-center">
+            <span class="text-warning fw-bold title-text-pricelist">MONTHLY</span>
+            <h3 style="letter-spacing:3px" class="text-white text-center text-pricelist">25 DAYS</h3>
+            <hr class="mx-auto line-pricelist">
+            <div class="text-white text-center">
+              <span class="fw-bold text-pricelist">LUNCH & DINNER</span><br>
+              <table class="table" style="border:none !important; border-style:hidden !important;">
+                <tbody>
+                  <tr>
+                    <td>
+                      <span class="text-warning text-promo fw-light margin-promo-icon"><span class="text-white">IDR</span> 2.500.000</span><br>
+                      <span class="text-warning text-pricelist2 margin-promo-icon"><span class="small-text">IDR</span> 2.399.000</span>
+                    </td>
+                    <td>
+                      <img src="img/promo.png" class="img-promo">
+                    </td>
+                  </tr>
+                </tbody>
+              </table>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="container mt-5">
+      <div class="row justify-content-between text-center">
+        <div class="position-additional col-5 text-center bg-yellow-additional py-2">
+          <span  class="text-additional">ADDITIONAL</span>
+        </div>
+        <div style="z-index:0" class="border-additional">
+          <div class="mt-5">
+            <span class="fw-bold text-pricelist text-warning mt-4">WEEKEND MEAL <span class="text-white"><br>( PRICE PER MEAL )</span></span><br>
+            <span class="text-warning text-pricelist"><span class="small-text">IDR</span> 50.000</span><br>
+          </div>
+          <hr class="mx-auto line-pricelist">
+          <div class="mt-4 mb-5">
+            <span class="fw-bold text-pricelist text-warning mt-4">MOOLES FIBER DRINK <br> <span class="text-white">( PRICE PER BOX 5 SACHET )</span></span><br>
+            <span class="text-warning text-pricelist"><span class="small-text">IDR</span> 85.000</span>
+          </div>
+        </div>
+      </div>
+      <div class="margin-discount text-center">
+        <span class="fw-bold text-pricelist text-white mt-4">DISCOUNT </span>
+        <span class="text-warning text-pricelist fw-bolder"><span class="small-text">IDR</span> 70.000</span><br>
+        <div class="row mt-1">
+          <div class="col-3 text-end p-0">
+            <hr class="mx-auto line-pricelist">
+          </div>
+          <div class="col-6">
+            <span class="text-white text-discount"> SETIAP AJAK 2 TEMAN </span>
+          </div>
+          <div class="col-3 text-start p-0">
+            <hr class="mx-auto line-pricelist">
+          </div>
+        </div>
+        <div>
+          <span class="text-decoration-underline text-white text-discount">( PENGIRIMAN HARUS KE ALAMAT YANG SAMA )</span>
+        </div>
+        <div>
+          <span class="text-warning text-pricelist fw-bold">FREE DELIVERY FEE</span>
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
+
+<!-- Footer Start -->
+<x-footer/>
+<!-- Footer End -->
+    <script src="https://yellowfitkitchen.com/frontend/js/bootstrap.min.js"></script>
+    <script src="https://yellowfitkitchen.com/frontend/js/theme.js"></script>
+    <script src="https://yellowfitkitchen.com/frontend/js/tambahan.js"></script>
+    <script src="https://yellowfitkitchen.com/app-assets/vendors/js/jquery/jquery.min.js"></script>
+    <script src="https://yellowfitkitchen.com/app-assets/vendors/js/extensions/sweetalert2.all.min.js"></script>
+    <script src="https://yellowfitkitchen.com/app-assets/js/scripts/extensions/ext-component-sweet-alerts.js"></script>
+    <script src="https://unpkg.com/aos@next/dist/aos.js"></script>
+    <script src="https://yellowfitkitchen.com/frontend/js/owl.carousel.min.js"></script>
+
+  </body>
+</html>
