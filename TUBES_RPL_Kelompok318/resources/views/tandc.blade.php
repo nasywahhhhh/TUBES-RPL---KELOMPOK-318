9eb47dc6b6bdefed08f3c201e8375fd3dedf10fb
nasywahhhhh <naswahputri88c@gmail.com>
2024-05-21 12:53:37 +0700

    TRK3-34

---
+++ TUBES_RPL_Kelompok318/resources/views/tandc.blade.php
@@ -0,0 +1,125 @@
+
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1" />
+    <meta name="events-id" content="NbrAYWDupkRk7kS5tRryh1sVyv0zfpaTCRIniNvl" />
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://yellowfitkitchen.com/frontend/css/bootstrap.min.css" />
+    <link rel="stylesheet" href="https://yellowfitkitchen.com/frontend/css/owl.carousel.min.css">
+    <link rel="stylesheet" href="https://yellowfitkitchen.com/frontend/css/owl.theme.default.min.css">
+    <title>Feater Catering Adalah Diet & Healthy Catering No.1 Di Indonesia</title>
+    <!-- Icon Font Stylesheet -->
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.10.0/css/all.min.css" rel="stylesheet">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.4.1/font/bootstrap-icons.css" rel="stylesheet">
+    <!-- Favicon -->
+    <link href="img/favicon.ico" rel="icon">
+    <!-- Customized Bootstrap Stylesheet -->
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+    <!-- Template Stylesheet -->
+    <link href="css/style1.css" rel="stylesheet">
+  </head>
+  <body>
+    <!-- BEGIN: Header-->
+<button type="button" class="btn btn-warning btn-floating" id="btn-back-to-top">
+  <i class="bi bi-arrow-up-circle fs-4"></i>
+</button>
+<!-- Navbar -->
+<x-navbar/>
+  <!-- END: Header-->
+
+<!-- MODAL -->
+<x-modal/>
+
+
+<div class="tnc-menu">
+  <div class="tnc-image">
+    <div class="banner-text-tnc text-center text-white">
+      <h1 class="fst-italic">TERM</h1>
+      <h1 class="fst-italic">AND</h1>
+      <h1 class="fst-italic">CONDITIONS</h1>
+    </div>
+  </div>
+  <div class="container">
+    <div class="row">
+      <div class="col-lg-6 mb-5">
+        <h2 class="text-white fst-italic fw-bold">
+          Ga Enak? <span class="text-warning">Refund!</span>
+        </h2>
+        <ul>
+          <li class="text-white mt-5 fst-italic">
+            Makanan dianggap tidak enak apabila rasanya hambar (karena tidak
+            enak itu relatif).
+          </li>
+          <li class="text-white mt-4 fst-italic">
+            Bila makanan hari tersebut dianggap hambar, bisa lapor ke admin
+            dan protes akan ditampung terlebih dahulu. Refund akan dilakukan
+            apabila protes masuk lebih dari 5% orderan hari tersebut. Misal
+            ada 2000 customer yang pesan, makan akan direfund bila ada lebih
+            dari 100 customer yang protes..
+          </li>
+          <li class="text-white mt-4 mb-4 fst-italic">
+            Uang yang direfund adalah sesuai jumlah makanan yang tidak enak.
+            Misal dari 10 menu, hanya 2 yang tidak enak, maka hanya 2 yang
+            direfund. Apabila semua dianggap tidak enak, boleh lapor ke admin
+            setiap hari.
+          </li>
+        </ul>
+      </div>
+      <div class="col-lg-6">
+        <h2 class="text-white fst-italic fw-bold">
+          Ga Enak? <span class="text-warning">Refund!</span>
+        </h2>
+        <ul>
+          <li class="text-white mt-5 fst-italic">
+            Hanya berlaku untuk paket lunch dan dinner.
+          </li>
+          <li class="text-white mt-4 fst-italic">
+            Hanya berlaku apabila customer tidak makan makanan lain selain
+            lunch dan dinner dari Feater Catering.
+          </li>
+          <li class="text-white mt-4 mb-4 fst-italic">
+            Weekend hanya boleh makan 2x porsi normal, tidak boleh makan
+            gorengan dan minum minuman manis.
+          </li>
+          <li class="text-white mt-4 mb-4 fst-italic">
+            Customer wajib membuat rekaman video timbangan berat badan before
+            after seluruh badan, direkam oleh orang lain dan harus ada tanggal
+            hari itu di video (bisa lewat tanggal di hp atau media lain).
+            Video dibuat dua kali di Senin pagi dan Sabtu pagi (disarankan
+            dalam keadaan yang sama, misal sehabis buang air besar).
+          </li>
+          <li class="text-white mt-4 mb-4 fst-italic">
+            Pembuktian dilakukan oleh team personal trainer Feater Catering dengan
+            metode hitung kebutuhan kalori harian dan kalori masuk.
+          </li>
+          <li class="text-white mt-4 mb-4 fst-italic">
+            Jika terbukti berbohong (misal cheat meal) tetapi minta refund,
+            maka akan diekspos dan dikenakan denda 1 juta rupiah.
+          </li>
+          <li class="text-white mt-4 mb-4 fst-italic">
+            Uang yang direfund adalah senilai total orderan customer. Misal
+            pesan weekly lunch dinner, maka akan direfund full.
+          </li>
+        </ul>
+      </div>
+    </div>
+  </div>
+</div>
+
+<!-- Footer Start -->
+<x-footer/>
+<!-- Footer End -->
+    <script src="https://yellowfitkitchen.com/frontend/js/bootstrap.min.js"></script>
+    <script src="https://yellowfitkitchen.com/frontend/js/theme.js"></script>
+    <script src="https://yellowfitkitchen.com/frontend/js/tambahan.js"></script>
+    <script src="https://yellowfitkitchen.com/app-assets/vendors/js/jquery/jquery.min.js"></script>
+    <script src="https://yellowfitkitchen.com/app-assets/vendors/js/extensions/sweetalert2.all.min.js"></script>
+    <script src="https://yellowfitkitchen.com/app-assets/js/scripts/extensions/ext-component-sweet-alerts.js"></script>
+    <script src="https://unpkg.com/aos@next/dist/aos.js"></script>
+    <script src="https://yellowfitkitchen.com/frontend/js/owl.carousel.min.js"></script>
+
+      </body>
+</html>
