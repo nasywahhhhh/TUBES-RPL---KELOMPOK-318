1ff96d9a47e2b88a7b3637c399d9c1cdf73ec2f7
dffailhmfkri <dilhamfik1@gmail.com>
2024-06-11 22:04:40 +0700

    TRK3-11

---
+++ TUBES_RPL_Kelompok318/resources/views/testicustomer.blade.php
@@ -0,0 +1,130 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <title>Testimonial</title>
+    <meta content="width=device-width, initial-scale=1.0" name="viewport">
+    <meta content="" name="keywords">
+    <meta content="" name="description">
+
+    <!-- Favicon -->
+    <link href="img/favicon.ico" rel="icon">
+
+    <!-- Google Web Fonts -->
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Heebo:wght@400;500;600&family=Nunito:wght@600;700;800&family=Pacifico&display=swap" rel="stylesheet">
+
+    <!-- Icon Font Stylesheet -->
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.10.0/css/all.min.css" rel="stylesheet">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.4.1/font/bootstrap-icons.css" rel="stylesheet">
+
+    <!-- Libraries Stylesheet -->
+    <link href="lib/animate/animate.min.css" rel="stylesheet">
+    <link href="lib/owlcarousel/assets/owl.carousel.min.css" rel="stylesheet">
+    <link href="lib/tempusdominus/css/tempusdominus-bootstrap-4.min.css" rel="stylesheet" />
+
+    <!-- Customized Bootstrap Stylesheet -->
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+
+    <!-- Template Stylesheet -->
+    <link href="css/style.css" rel="stylesheet">
+    <style>
+        body {
+      background-color: #262D3F;
+    }
+    </style>
+</head>
+
+<body>
+     <!-- Navbar -->
+     <x-navbar/>
+
+     <!-- Modal -->
+     <x-modal />
+     <br><br><br><br>
+         <!-- Jumbotron with Our Product title -->
+    <div class="jumbotron text-center">
+        <h1 class="display-4" style="font-weight: bold; color: #FEA116;">TESTIMONIAL</h1>
+    </div>
+        <!-- Navbar & Hero End -->
+        <!-- Testimonial Start -->
+        <div class="main-article">
+            <div class="text-white latest-article">
+              <div class="container px-lg-5 pt-lg-5">
+                <h3 class="whitext" style="color: #FEA116;">Our Customer Say!!!</h5>
+                </div>
+                </div>
+                <div class="owl-carousel testimonial-carousel">
+                    <div class="testimonial-item bg-transparent border rounded p-4">
+                        <i class="fa fa-quote-left fa-2x text-primary mb-3"></i>
+                        <p style="color: white">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Facere voluptatum illo asperiores nam illum impedit adipisci voluptas, expedita pariatur repudiandae architecto, saepe sunt inventore explicabo nemo vitae, recusandae porro quo.</p>
+                        <div class="d-flex align-items-center">
+                            <img class="img-fluid flex-shrink-0 rounded-circle" src="https://6.soompi.io/wp-content/uploads/image/20240602151347_Jungkook.jpg?s=900x600&e=t" style="width: 75px; height: 75px;">
+                            <div class="ps-3">
+                                <h5 class="mb-1" style="color: #FEA116">Daffa Andika</h5>
+                                <small style="font-weight: bold; color: white ">Content Creator</small>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="testimonial-item bg-transparent border rounded p-4">
+                        <i class="fa fa-quote-left fa-2x text-primary mb-3"></i>
+                        <p style="color: white">Lorem ipsum dolor sit amet consectetur adipisicing elit. Mollitia inventore velit, aliquam cumque consequuntur cum quasi temporibus in amet est? Rem harum necessitatibus, quo in voluptate velit eos magni incidunt!</p>
+                        <div class="d-flex align-items-center">
+                            <img class="img-fluid flex-shrink-0 rounded-circle" src="https://asset.kompas.com/crops/l5cT--7ivmGImnz5SC2wAPk4140=/0x55:640x481/750x500/data/photo/2020/06/02/5ed6356238851.jpg" style="width: 75px; height: 75px;">
+                            <div class="ps-3">
+                                <h5 class="mb-1" style="color: #FEA116">Alif Fajar Maulana</h5>
+                                <small  style="font-weight: bold; color: white ">Selebgram</small>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="testimonial-item bg-transparent border rounded p-4">
+                        <i class="fa fa-quote-left fa-2x text-primary mb-3"></i>
+                        <p style="color: white">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Dignissimos atque qui amet dicta assumenda. Quam in, numquam enim dicta recusandae esse adipisci beatae delectus saepe accusamus, harum explicabo sed voluptatum?</p>
+                        <div class="d-flex align-items-center">
+                            <img class="img-fluid flex-shrink-0 rounded-circle" src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSRitKgNVtTFXZRWKcKqmFKySjO7c4auK4KDg&s" style="width: 75px; height: 75px;">
+                            <div class="ps-3"> 
+                                <h5 class="mb-1" style="color: #FEA116">Jizdan Rizkairdi</h5>
+                                <small style="font-weight: bold; color: white ">Content Creator</small>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="testimonial-item bg-transparent border rounded p-4">
+                        <i class="fa fa-quote-left fa-2x text-primary mb-3"></i>
+                        <p  style="color: white">Lorem ipsum dolor sit amet consectetur adipisicing elit. Iste, cumque! Fuga, temporibus dicta! Earum, et quos eos, sit unde, impedit deserunt ipsum inventore voluptate hic eum totam! Laudantium, sequi consequatur!  </p>
+                        <div class="d-flex align-items-center">
+                            <img class="img-fluid flex-shrink-0 rounded-circle" src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSyt_wPnUSZZehxuVuAv282JgIhT9lrO-pPxA&s" style="width: 75px; height: 75px;">
+                            <div class="ps-3">
+                                <h5 class="mb-1" style="color: #FEA116">Noor Panca Maulana</h5>
+                                <small style="font-weight: bold; color: white ">Selebgram</small>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <!-- Testimonial End -->
+        <!-- Footer -->
+        <x-footer style="background-color: #262D3F;"></x-footer>
+        <!-- Back to Top -->
+        <a href="#" class="btn btn-lg btn-primary btn-lg-square back-to-top"><i class="bi bi-arrow-up"></i></a>
+    </div>
+
+    <!-- JavaScript Libraries -->
+    <script src="https://code.jquery.com/jquery-3.4.1.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0/dist/js/bootstrap.bundle.min.js"></script>
+    <script src="lib/wow/wow.min.js"></script>
+    <script src="lib/easing/easing.min.js"></script>
+    <script src="lib/waypoints/waypoints.min.js"></script>
+    <script src="lib/counterup/counterup.min.js"></script>
+    <script src="lib/owlcarousel/owl.carousel.min.js"></script>
+    <script src="lib/tempusdominus/js/moment.min.js"></script>
+    <script src="lib/tempusdominus/js/moment-timezone.min.js"></script>
+    <script src="lib/tempusdominus/js/tempusdominus-bootstrap-4.min.js"></script>
+
+    <!-- Template Javascript -->
+    <script src="js/main.js"></script>
+</body>
+
+</html>
---
+++ TUBES_RPL_Kelompok318/resources/views/testiinfluencer.blade.php
@@ -0,0 +1,194 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <title>Testimonial</title>
+    <meta content="width=device-width, initial-scale=1.0" name="viewport">
+    <meta content="" name="keywords">
+    <meta content="" name="description">
+
+    <!-- Favicon -->
+    <link href="img/favicon.ico" rel="icon">
+
+    <!-- Google Web Fonts -->
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Heebo:wght@400;500;600&family=Nunito:wght@600;700;800&family=Pacifico&display=swap" rel="stylesheet">
+
+    <!-- Icon Font Stylesheet -->
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.10.0/css/all.min.css" rel="stylesheet">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.4.1/font/bootstrap-icons.css" rel="stylesheet">
+
+    <!-- Libraries Stylesheet -->
+    <link href="lib/animate/animate.min.css" rel="stylesheet">
+    <link href="lib/owlcarousel/assets/owl.carousel.min.css" rel="stylesheet">
+    <link href="lib/tempusdominus/css/tempusdominus-bootstrap-4.min.css" rel="stylesheet" />
+
+    <!-- Customized Bootstrap Stylesheet -->
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+
+    <!-- Template Stylesheet -->
+    <link href="css/style.css" rel="stylesheet">
+    <style>
+        body {
+      background-color: #262D3F;
+    }
+    </style>
+</head>
+
+<body>
+     <!-- Navbar -->
+     <x-navbar/>
+
+     <!-- Modal -->
+     <x-modal />
+     <br><br><br><br>
+         <!-- Jumbotron with Our Product title -->
+    <div class="jumbotron text-center">
+        <h1 class="display-4" style="font-weight: bold; color: #FEA116;">TESTIMONIAL</h1>
+    </div>
+  
+        <!-- Navbar & Hero End -->
+        <!-- Testimonial Start -->
+        <div class="main-article">
+            <div class="text-white latest-article">
+              <div class="container px-lg-5 pt-lg-5">
+                <h3 class="whitext" style="color: #FEA116;">Our Influencer Say!!!</h5>
+                </div>
+                <div class="owl-carousel testimonial-carousel">
+                    <div class="testimonial-item bg-transparent border rounded p-4">
+                        <i class="fa fa-quote-left fa-2x text-primary mb-3"></i>
+                        <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Facere voluptatum illo asperiores nam illum impedit adipisci voluptas, expedita pariatur repudiandae architecto, saepe sunt inventore explicabo nemo vitae, recusandae porro quo.</p>
+                        <div class="d-flex align-items-center">
+                            <img class="img-fluid flex-shrink-0 rounded-circle" src="https://awsimages.detik.net.id/visual/2019/08/28/de542014-5a76-4de2-802b-e4a65bcfec57_169.jpeg?w=650" style="width: 75px; height: 75px;">
+                            <div class="ps-3">
+                                <h5 class="mb-1" style="color: #FEA116">Deddy Courbuzier</h5>
+                                <small style="font-weight: bold;">Content Creator</small>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="testimonial-item bg-transparent border rounded p-4">
+                        <i class="fa fa-quote-left fa-2x text-primary mb-3"></i>
+                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Mollitia inventore velit, aliquam cumque consequuntur cum quasi temporibus in amet est? Rem harum necessitatibus, quo in voluptate velit eos magni incidunt!</p>
+                        <div class="d-flex align-items-center">
+                            <img class="img-fluid flex-shrink-0 rounded-circle" src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT-XQ0VQPMkxsRfDmi1a_lCvPH2ljEj5TQnqg&s" style="width: 75px; height: 75px;">
+                            <div class="ps-3">
+                                <h5 class="mb-1"  style="color: #FEA116">Awkarin</h5>
+                                <small  style="font-weight: bold;">Selebgram</small>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="testimonial-item bg-transparent border rounded p-4">
+                        <i class="fa fa-quote-left fa-2x text-primary mb-3"></i>
+                        <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Dignissimos atque qui amet dicta assumenda. Quam in, numquam enim dicta recusandae esse adipisci beatae delectus saepe accusamus, harum explicabo sed voluptatum?</p>
+                        <div class="d-flex align-items-center">
+                            <img class="img-fluid flex-shrink-0 rounded-circle" src="https://media.suara.com/pictures/original/2021/05/24/62852-azka-corbuzier.jpg" style="width: 75px; height: 75px;">
+                            <div class="ps-3"> 
+                                <h5 class="mb-1" style="color: #FEA116">Azka Courbuzier</h5>
+                                <small style="font-weight: bold;">Content Creator</small>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="testimonial-item bg-transparent border rounded p-4">
+                        <i class="fa fa-quote-left fa-2x text-primary mb-3"></i>
+                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Iste, cumque! Fuga, temporibus dicta! Earum, et quos eos, sit unde, impedit deserunt ipsum inventore voluptate hic eum totam! Laudantium, sequi consequatur!  </p>
+                        <div class="d-flex align-items-center">
+                            <img class="img-fluid flex-shrink-0 rounded-circle" src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRFcg6iQVExuW6NKj3aj0BEIIwBf9ioEs1SDQ&s" style="width: 75px; height: 75px;">
+                            <div class="ps-3">
+                                <h5 class="mb-1"  style="color: #FEA116">Andrea Dian</h5>
+                                <small style="font-weight: bold;">Selebgram</small>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <!-- Testimonial End -->
+
+        <!-- Footer Start -->
+        <div class="container-fluid bg-dark text-light footer pt-5 mt-5 wow fadeIn" data-wow-delay="0.1s">
+            <div class="container py-5">
+                <div class="row g-5">
+                    <div class="col-lg-3 col-md-6">
+                        <h4 class="section-title ff-secondary text-start text-primary fw-normal mb-4">Company</h4>
+                        <a class="btn btn-link" href="">About Us</a>
+                        <a class="btn btn-link" href="">Contact Us</a>
+                        <a class="btn btn-link" href="">Reservation</a>
+                        <a class="btn btn-link" href="">Privacy Policy</a>
+                        <a class="btn btn-link" href="">Terms & Condition</a>
+                    </div>
+                    <div class="col-lg-3 col-md-6">
+                        <h4 class="section-title ff-secondary text-start text-primary fw-normal mb-4">Contact</h4>
+                        <p class="mb-2"><i class="fa fa-map-marker-alt me-3"></i>123 Street, New York, USA</p>
+                        <p class="mb-2"><i class="fa fa-phone-alt me-3"></i>+012 345 67890</p>
+                        <p class="mb-2"><i class="fa fa-envelope me-3"></i>info@example.com</p>
+                        <div class="d-flex pt-2">
+                            <a class="btn btn-outline-light btn-social" href=""><i class="fab fa-twitter"></i></a>
+                            <a class="btn btn-outline-light btn-social" href=""><i class="fab fa-facebook-f"></i></a>
+                            <a class="btn btn-outline-light btn-social" href=""><i class="fab fa-youtube"></i></a>
+                            <a class="btn btn-outline-light btn-social" href=""><i class="fab fa-linkedin-in"></i></a>
+                        </div>
+                    </div>
+                    <div class="col-lg-3 col-md-6">
+                        <h4 class="section-title ff-secondary text-start text-primary fw-normal mb-4">Opening</h4>
+                        <h5 class="text-light fw-normal">Monday - Saturday</h5>
+                        <p>09AM - 09PM</p>
+                        <h5 class="text-light fw-normal">Sunday</h5>
+                        <p>10AM - 08PM</p>
+                    </div>
+                    <div class="col-lg-3 col-md-6">
+                        <h4 class="section-title ff-secondary text-start text-primary fw-normal mb-4">Newsletter</h4>
+                        <p>Dolor amet sit justo amet elitr clita ipsum elitr est.</p>
+                        <div class="position-relative mx-auto" style="max-width: 400px;">
+                            <input class="form-control border-primary w-100 py-3 ps-4 pe-5" type="text" placeholder="Your email">
+                            <button type="button" class="btn btn-primary py-2 position-absolute top-0 end-0 mt-2 me-2">SignUp</button>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="container">
+                <div class="copyright">
+                    <div class="row">
+                        <div class="col-md-6 text-center text-md-start mb-3 mb-md-0">
+                            &copy; <a class="border-bottom" href="#">Your Site Name</a>, All Right Reserved. 
+							
+							<!--/*** This template is free as long as you keep the footer author’s credit link/attribution link/backlink. If you'd like to use the template without the footer author’s credit link/attribution link/backlink, you can purchase the Credit Removal License from "https://htmlcodex.com/credit-removal". Thank you for your support. ***/-->
+							Designed By <a class="border-bottom" href="https://htmlcodex.com">HTML Codex</a>
+                        </div>
+                        <div class="col-md-6 text-center text-md-end">
+                            <div class="footer-menu">
+                                <a href="">Home</a>
+                                <a href="">Cookies</a>
+                                <a href="">Help</a>
+                                <a href="">FQAs</a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <!-- Footer End -->
+
+
+        <!-- Back to Top -->
+        <a href="#" class="btn btn-lg btn-primary btn-lg-square back-to-top"><i class="bi bi-arrow-up"></i></a>
+    </div>
+
+    <!-- JavaScript Libraries -->
+    <script src="https://code.jquery.com/jquery-3.4.1.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0/dist/js/bootstrap.bundle.min.js"></script>
+    <script src="lib/wow/wow.min.js"></script>
+    <script src="lib/easing/easing.min.js"></script>
+    <script src="lib/waypoints/waypoints.min.js"></script>
+    <script src="lib/counterup/counterup.min.js"></script>
+    <script src="lib/owlcarousel/owl.carousel.min.js"></script>
+    <script src="lib/tempusdominus/js/moment.min.js"></script>
+    <script src="lib/tempusdominus/js/moment-timezone.min.js"></script>
+    <script src="lib/tempusdominus/js/tempusdominus-bootstrap-4.min.js"></script>
+
+    <!-- Template Javascript -->
+    <script src="js/main.js"></script>
+</body>
+
+</html>
