4cef3e8e8c659e749a3b42dd38aa6889229fa346
CIKAYA <115714689+CIKAYA@users.noreply.github.com>
2024-06-11 21:48:20 +0700

    TRK3-3

---
+++ TUBES_RPL_Kelompok318/resources/views/home.blade.php
@@ -0,0 +1,324 @@
+
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1" />
+    <meta name="events-id" content="uRXCrJFKxYJdmoD763X7xZNa5dq1S1j5Ulk1u7Rn" />
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://yellowfitkitchen.com/frontend/css/bootstrap.min.css" />
+    <link rel="stylesheet" href="https://unpkg.com/aos@next/dist/aos.css" />
+    <link rel="stylesheet" href="https://yellowfitkitchen.com/frontend/css/owl.carousel.min.css">
+    <link rel="stylesheet" href="https://yellowfitkitchen.com/frontend/css/owl.theme.default.min.css">
+    <title>Home Feater Catering</title>
+    <!-- Icon Font Stylesheet -->
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.10.0/css/all.min.css" rel="stylesheet">
+    <!-- Favicon -->
+    <!-- Menu-->
+    <link href="img/favicon.ico" rel="icon">
+    <!-- Customized Bootstrap Stylesheet -->
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+    <!-- Template Stylesheet -->
+    <link href="css/style1.css" rel="stylesheet">
+    <script type="text/javascript">
+      var onloadCallback = function() {
+        grecaptcha.enterprise.render('g-recaptcha', {
+          'sitekey' : '6LdF0J0kAAAAAMO-4HtmllEu1IfmvIIq98SjAqla',
+        });
+      };
+    </script>
+    <meta name="description" content="Program YellowFit Menawarkan Solusi Mudah Bagi Masyarakat Indonesia Untuk Diet Dan Hidup Sehat | Jaminan Uang Kembali 100% | Nutritionist approved | Affordable Price | Free Delivery Fee | Rasa Dijamin Lebih Enak | Konsultasi Gratis">
+
+
+    <noscript><img height="1" width="1" src="https://www.facebook.com/tr?id=306444486952151&ev=PageView&noscript=1"/></noscript>
+
+    <style>
+      .first_button {
+        animation: shake 0.82s cubic-bezier(.36, .07, .19, .97) both infinite;
+        transform: translate3d(0, 0, 0);
+        backface-visibility: hidden;
+        perspective: 1000px;
+      }
+      body {
+        background-color: #262d3f;
+    }
+      @keyframes  shake {
+        10%, 90% { transform: translate3d(-1px, 0, 0); }
+        20%, 80% { transform: translate3d(2px, 0, 0); }
+        30%, 50%, 70% { transform: translate3d(-4px, 0, 0); }
+        40%, 60% { transform: translate3d(4px, 0, 0); }
+      }
+    </style>
+  </head>
+  <body>
+    <!-- BEGIN: Header-->
+    <div class=" bg-white p-0">
+        <!-- Navbar & Hero Start -->
+        <div class="position-relative p-0">
+            <x-navbar/>
+
+            <div class="py-5  hero-header mb-5">
+                <div class="container my-5 py-5">
+                    <div class="row align-items-center g-5">
+                        <div class="col-lg-6 text-center text-lg-start">
+                        <h1 class="display-3" style="color: #FEA116; font-weight: 600;">FEATER<br>CATERING</h1>
+                            <h5 class="mb-4 pb-2" style="color: #FF9E0C; font-weight: 400;">Mari hidup sehat bersama kami dengan pelayanan yang didambakan setiap orang</h5>
+                            <div class="about_bt"><a href="{{route('jadwal.index')}}">Menu Minggu Depan</a></div>
+                            <div class="contact_bt"><a href="{{route('konsultasi')}}">Konsultasi Gratis</a></div>
+                        </div>
+                        <div class="col-lg-6 text-center text-lg-end overflow-hidden">
+                            <img class="img-fluid" src="img/bg-lingkaran.png" alt="">
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <!-- Navbar & Hero End -->
+
+<!-- MODAL -->
+<x-modal/>
+<!-- About Start -->
+<div class="container-xxl py-5">
+    <div class="container">
+        <div class="row g-5 align-items-center">
+            <div class="col-lg-6">
+                <div class="row g-3">
+                    <div class="col-6 text-start">
+                        <img class="img-fluid rounded w-100" src="img/about-1.jpg">
+                    </div>
+                    <div class="col-6 text-start">
+                        <img class="img-fluid rounded w-75" src="img/about-2.jpg" style="margin-top: 25%;">
+                    </div>
+                    <div class="col-6 text-end">
+                        <img class="img-fluid rounded w-75" src="img/about-3.jpg">
+                    </div>
+                    <div class="col-6 text-end">
+                        <img class="img-fluid rounded w-100" src="img/about-4.jpg">
+                    </div>
+                </div>
+            </div>
+            <div class="col-lg-6">
+                <div class="cprogram">
+                <h2 class="mb-4"><i class="fa fa-utensils text-primary me-2"></i>Feater Catering Program</h2>
+                <p class="mb-4">Hai teman hidup sehat! Untuk kamu yang memiliki kendala berat badan dan hidup sehat, saat ini kami hadir untuk menjadi solusi anda menjadi lebih baik lagi.</p>
+                <p class="mb-4">Kami memberikan pengalaman hidup sehat terbaik untuk kamu coba, mulai dari menu yang beragam, hingga bahan makanan yang tentunya berkualitas juga disetiap masakan.</p>
+                <p class="mb-4">Tenang! Kami sudah memiliki lisensi Halal dan direkomendasikan oleh dokter Gizi ternama di Indonesia. Kamu tidak perlu khawatir untuk menikmati pengalaman baru.</p>
+                <p class="mb-4">Koki FEATER CATERING siap memasak dengan proseional tanpa harus kamu khawatirkan tentang kualitas dan kebersihan makanan yang akan kamu nikmati.</p>
+                <div class="row g-4 mb-4">
+                    <div class="col-sm-6">
+                        <div class="d-flex align-items-center border-start border-5 border-primary px-3">
+                            <h1 class="flex-shrink-0 display-5 text-primary mb-0">10,450</h1>
+                            <div class="ps-4">
+                                <p class="mb-0">Total Customer</p>
+                                <h6 class="text-uppercase mb-0">2024</h6>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-sm-6">
+                        <div class="d-flex align-items-center border-start border-5 border-primary px-3">
+                            <h1 class="flex-shrink-0 display-5 text-primary mb-0">6,500</h1>
+                            <div class="ps-4">
+                                <p class="mb-0">Total Customer</p>
+                                <h6 class="text-uppercase mb-0">Bulan ini</h6>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+<!-- About End -->
+
+ <!-- Service Start -->
+ <div class="container-xxl py-5">
+    <div class="container">
+        <div class="row g-4 text-center">
+            <div class="col-lg-4 col-sm-6">
+                <div class="service-item rounded pt-3">
+                    <div class="p-4">
+                        <i class="fa fa-3x fa-university text-primary mb-4"></i>
+                        <h5>20 Maret 2024</h5>
+                        <p>Opening Kitchen</p>
+                    </div>
+                </div>
+            </div>
+            <div class="col-lg-4 col-sm-6">
+                <div class="service-item rounded pt-3">
+                    <div class="p-4">
+                        <i class="fa fa-3x fa-utensils text-primary mb-4"></i>
+                        <h5>18 Outlet</h5>
+                        <p>Seluruh Indonesia</p>
+                    </div>
+                </div>
+            </div>
+            <div class="col-lg-4 col-sm-6 wow">
+                <div class="service-item rounded pt-3">
+                    <div class="p-4">
+                        <i class="fa fa-3x fa-trophy text-primary mb-4"></i>
+                        <h5>Refund Uang</h5>
+                        <p>Jika tidak turun berat badan</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+<!-- Service End -->
+
+
+<!-- Testi Start -->
+<div class="container-xxl pt-5 pb-3">
+    <div class="container">
+        <div class="text-center">
+            <h1 class="mb-5">Our Testimoni</h1>
+        </div>
+        <div class="row g-4">
+            <div class="col-lg-3 col-md-6">
+                <div class="team-item text-center rounded">
+                    <div class="rounded-circle m-4">
+                        <img class="img-fluid" src="img/testi-1.jpg" alt="">
+                    </div>
+                    <h5 class="mb-0">Clarissa Putri</h5>
+                    <small>Brand Ambassador</small>
+                    <div class="d-flex justify-content-center mt-3">
+                    </div>
+                </div>
+            </div>
+            <div class="col-lg-3 col-md-6">
+                <div class="team-item text-center rounded">
+                    <div class="rounded-circle m-4">
+                        <img class="img-fluid" src="img/testi-2.jpg" alt="">
+                    </div>
+                    <h5 class="mb-0">Rachel Vennya</h5>
+                    <small>Selebgram</small>
+                    <div class="d-flex justify-content-center mt-3">
+                    </div>
+                </div>
+            </div>
+            <div class="col-lg-3 col-md-6">
+                <div class="team-item text-center rounded">
+                    <div class="rounded-circle m-4">
+                        <img class="img-fluid" src="img/testi-3.jpg" alt="">
+                    </div>
+                    <h5 class="mb-0">Fadil Jaidi</h5>
+                    <small>Selebgram</small>
+                    <div class="d-flex justify-content-center mt-3">
+                    </div>
+                </div>
+            </div>
+            <div class="col-lg-3 col-md-6">
+                <div class="team-item text-center rounded">
+                    <div class="rounded-circle m-4">
+                        <img class="img-fluid" src="img/testi-4.jpg" alt="">
+                    </div>
+                    <h5 class="mb-0">Deddy Courbuzier</h5>
+                    <small>Selebriti</small>
+                    <div class="d-flex justify-content-center mt-3">
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+<!-- Testi End -->
+
+<!-- Location Start -->
+<div class="container-xxl py-5"></div>
+    <div class="container">
+        <div class="text-center">
+            <div class="row g-5 align-items-center">
+                <div class="">
+                    <h1 class="mb-5">Our Kitchen</h1>
+                    <p class="mb-4">Jakarta, Jl. Panglima Polim No.125 D, RT.2/RW.1, Pulo, Kec. Kby. Baru, Kota Jakarta Selatan, Daerah Khusus Ibukota Jakarta 12160</p>
+                    <p class="mb-4">Bandung, Jl. Cikawao Dalam I No.35 A, Paledang, Kec. Lengkong, Kota Bandung, Jawa Barat 40261</p>
+                 </div>
+            </div>
+        </div>
+    </div>
+</div>
+<!-- About End -->
+
+
+<!-- About Start -->
+<div class="container-xxl py-5">
+    <div class="container">
+        <div class="row g-2 align-items-center">
+            <img src="img/dapur1.jpeg" width="100" height="500"><br>
+            <p class="mb-4" style="font-weight: bold; text-align: center; color: #fea116; font-size: 35px;">Selamat Datang di Dapur Kami</p>
+            <p style="color: #ffffff; text-align: center;">Di Feater Catering, kami percaya bahwa gaya hidup sehat dimulai dari makanan bergizi dan lezat yang disiapkan dengan penuh perhatian dan dedikasi. Dapur kami adalah jantung dari operasi kami, tempat keahlian kuliner bertemu dengan bahan-bahan terbaik untuk menciptakan hidangan yang tidak hanya menyehatkan tubuh Anda tetapi juga memanjakan lidah Anda. Berikut adalah hal-hal yang membuat dapur kami istimewa: </p>  
+            <img src="img/sayur.png.jpg" width="100" height="500"><br>
+            <p class="mb-4" style="font-weight: bold; text-align: center; color: #fea116; font-size: 35px;">Bahan Segar dan Berkualitas Tinggi
+</p>
+            <p style="color: #ffffff; text-align: center;">Kami mendapatkan bahan-bahan yang segar dan berkualitas tinggi untuk memastikan setiap hidangan kaya akan nutrisi dan cita rasa. Kemitraan kami dengan petani lokal dan pemasok terpercaya memungkinkan kami untuk menyajikan sayuran organik, daging bebas hormon, dan seafood yang berkelanjutan. Kami percaya bahwa hidangan terbaik dimulai dari bahan-bahan terbaik.</p>   
+           
+        </div><br>
+        <div class="pola" style = "color: white"> 
+            <h3 class="mb-4 text-center" style = "color:#FEA116"><i class="text-primary"></i>Cara Atur Pola Makan Selama Bulan Puasa untuk Capai Berat Badan Ideal</h3>
+            <p>Bulan Ramadhan adalah momen yang sangat dinanti-nantikan oleh umat Muslim di seluruh dunia. Selain sebagai ibadah, puasa juga menjadi waktu yang tepat bagi seseorang untuk merubah kebiasaan makan dan gaya hidup, termasuk dalam menurunkan berat badan.</p>
+            <p>Namun, untuk mencapai tujuan tersebut, diperlukan perencanaan yang matang mengenai pola makan selama bulan puasa. Dalam artikel ini, Millow akan membahas bagaimana cara mengatur pola makan selama bulan puasa agar dapat mencapai berat badan ideal.</p>
+            <P>Membuat perencanaan menu makanan selama bulan puasa sangat penting untuk membantu mengatur asupan nutrisi harian. Pastikan menu makanan yang disiapkan seimbang dengan kandungan karbohidrat, protein, lemak, vitamin, dan mineral yang diperlukan oleh tubuh. Makanan yang rendah lemak dan gula, serta kaya serat akan membantu menurunkan berat badan dengan cepat.</P>
+            <p>Waktu makan sangat penting dalam mengatur pola makan selama bulan puasa. Hindari makan sahur terlalu larut malam karena hal ini dapat mengganggu kualitas tidur dan membuat rasa lapar sulit dikendalikan pada siang hari. Selain itu, hindari juga makan berlebihan saat berbuka karena dapat membuat perut kembung dan tidak nyaman.</p>
+            <p>Makanan berlemak dan berkalori tinggi adalah musuh utama dalam usaha menurunkan berat badan. Selama bulan puasa, hindari mengonsumsi makanan yang digoreng, berlemak, dan mengandung gula berlebihan. Pilihlah makanan yang direbus, dibakar, atau kukus dan pilih juga makanan yang rendah kalori, seperti sayur-sayuran.</p>
+            <P>Makanan yang disajikan selama berbuka dan sahur haruslah seimbang. Hindari makan berlebihan karena akan membuat perut kembung dan sulit dicerna. Sebaliknya, konsumsi makanan dalam porsi yang cukup dan diimbangi dengan minuman yang cukup seperti air putih atau jus buah.</P>
+            <p>Penting untuk tetap terhidrasi selama bulan puasa, terutama saat berbuka puasa. Usahakan untuk mengonsumsi air putih yang cukup setelah berbuka puasa hingga menjelang waktu sahur. Air putih dapat membantu menjaga keseimbangan cairan dalam tubuh, sehingga membantu mengontrol nafsu makan.</p>
+            <p>Dalam rangka mencapai berat badan ideal, diperlukan konsistensi dalam menjalankan pola makan yang seimbang selama bulan puasa. Dengan memperhatikan asupan makanan dan tetap terhidrasi dengan baik. Perencanaan pola makan yang tepat dan disiplin dalam menjalankan rencana tersebut akan membantu mencapai tujuan tersebut.</p> </div> <br>
+            <div class="about_bt"><a href="article.html">See more article</a></div>
+        </div>
+    </div>
+</div>
+<!-- About End -->
+
+<x-footer/>
+
+
+    <script src="https://yellowfitkitchen.com/frontend/js/bootstrap.min.js"></script>
+    <script src="https://yellowfitkitchen.com/frontend/js/theme.js"></script>
+    <script src="https://yellowfitkitchen.com/frontend/js/tambahan.js"></script>
+    <script src="https://yellowfitkitchen.com/app-assets/vendors/js/jquery/jquery.min.js"></script>
+    <script src="https://yellowfitkitchen.com/app-assets/vendors/js/extensions/sweetalert2.all.min.js"></script>
+    <script src="https://yellowfitkitchen.com/app-assets/js/scripts/extensions/ext-component-sweet-alerts.js"></script>
+    <script src="https://unpkg.com/aos@next/dist/aos.js"></script>
+    <script src="https://yellowfitkitchen.com/frontend/js/owl.carousel.min.js"></script>
+    <script>
+      $('.owl-carousel').owlCarousel({
+        autoplay:true,
+        loop:true,
+        margin:10,
+        nav:true,
+        responsive:{
+            300:{ items:1, nav:false },
+            600:{ items:1, nav:false },
+            900:{ items:2 },
+            1000:{ items:2 },
+        }
+    })
+    AOS.init();
+    </script>
+
+
+    <script>
+    $(document).on('click','.btn-weight-loss',function(e){
+      $('.btn-weight-loss').removeClass('breathing-button');
+      $('.btn-weight-loss-plus').addClass('breathing-button');
+      $('.weight-loss').removeClass('btn-danger1').addClass('btn-danger1-active');
+      $('.weight-loss-plus').removeClass('btn-danger1-active').addClass('btn-danger1');
+      e.preventDefault();
+        document.getElementById("testimoni-weightloss").style.display = "block";
+        document.getElementById("testimoni-weightloss-plus").style.display = "none";
+    });
+
+    $(document).on('click','.btn-weight-loss-plus',function(e){
+      $('.btn-weight-loss').addClass('breathing-button');
+      $('.btn-weight-loss-plus').removeClass('breathing-button');
+      $('.weight-loss').removeClass('btn-danger1-active').addClass('btn-danger1');
+      $('.weight-loss-plus').removeClass('btn-danger1').addClass('btn-danger1-active');
+      e.preventDefault();
+        document.getElementById("testimoni-weightloss").style.display = "none";
+        document.getElementById("testimoni-weightloss-plus").style.display = "block";
+    });
+</script>
+  </body>
+</html>
